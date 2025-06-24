5b39700a524a67608168670ff4c7c861f33a4877
Rostyslav-afk <110056857+Rostyslav-afk@users.noreply.github.com>
2025-06-24 12:05:46 +0200

    Header
    html full, and styles without flexbox

---
+++ js/header.js
@@ -0,0 +1,10 @@
+// //DARK THEME
+// const toggle = document.querySelector(".header__theme-togle");
+// const container = document.querySelector(".container");
+
+// toggle.addEventListener("click", (event) => {
+//     event.preventDefault();
+
+//     container.style.backgroundColor = "#000";
+//     container.style.color = "#fff";
+// });
